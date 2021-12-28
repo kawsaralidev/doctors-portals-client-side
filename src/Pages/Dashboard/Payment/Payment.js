1eea21fa058d75b6b946df99194acf923ac74530
kawsaralidev <mdkawsarali346@gmail.com>
2021-12-28 18:49:18 +0600

    done

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -12,7 +12,7 @@ const Payment = () => {
   const { appointmentId } = useParams();
   const [appointment, setAppointment] = useState({});
   useEffect(() => {
-    fetch(`http://localhost:5000/appointments/${appointmentId}`)
+    fetch(`https://secure-earth-73886.herokuapp.com/appointments/${appointmentId}`)
       .then((res) => res.json())
       .then((data) => setAppointment(data));
   }, [appointmentId]);
--- src/Pages/Home/Doctors/Doctors.js
+++ src/Pages/Home/Doctors/Doctors.js
@@ -6,7 +6,7 @@ const Doctors = () => {
   const [doctors, setDoctors] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/doctors")
+    fetch("https://secure-earth-73886.herokuapp.com/doctors")
       .then((res) => res.json())
       .then((data) => setDoctors(data));
   }, []);
